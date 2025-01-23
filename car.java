be636245f9195c7dc9ee7adb47bd197ef2198420
andyvungoc <149588666+andyvungoc@users.noreply.github.com>
2025-01-23 09:58:43 +0100

    move,turnrigh/left

--- car.java
+++ car.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 
-public abstract class car implements movable{
+public abstract class car {
     //Bör alla vara privata för att inte kunna ändras direkt av användaren. De är specifika för bilarna.
     private int nrDoors;
     protected double enginePower;
@@ -8,18 +8,8 @@ public abstract class car implements movable{
     private Color color;
     private String modelName;
     protected int direction = 0;
-
-    public void move(){
-
-    }
-
-    public void turnLeft(){
-
-    }
-
-    public void turnRight(){
-
-    }
+    protected double x = 0;
+    protected double y = 0;
 
     public car(int nrDoors, double enginePower, Color color, String modelName){
         this.nrDoors = nrDoors;
@@ -40,13 +30,12 @@ public abstract class car implements movable{
 
     // protected gömmer funktionerna från utsidan men gör de tillgängliga för subklasser.
     // abstract betyder att subklasser som ex volvo240 can använda denna funktion men att de definierar den unik för varje "fordon".
-    // LÄGG TILL check currentspeed inom [0, enginepower]
     protected abstract double speedFactor();
     protected void incrementSpeed(double amount){
         currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
     }
-    protected void decrementSpeed(double amount){
-        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
+    protected void decrementSpeed(double amount) {
+        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);
     }
 
     public void gas(double amount) {
@@ -61,5 +50,28 @@ public abstract class car implements movable{
             throw new IllegalArgumentException("Not accepted amount break, insert a value between 0 and 1");
         }
         decrementSpeed(amount);
+    }
+}
+
+    public void move() {
+        if (direction == 0) {
+            y += currentSpeed;
+        } else if (direction == 1) {
+            x += currentSpeed;
+        } else if (direction == 2) {
+            y -= currentSpeed;
+        } else if (direction == 3) {
+            x -= currentSpeed;
+        } else {
+            throw new IllegalStateException("Not a valid direction (0, 1, 2, 3)");
+        }
+    }
+
+    public void turnRight() {
+        direction = (direction + 1) % 4;
+    }
+
+    public void turnLeft() {
+        direction = (direction + 3) % 4;
     }
 }
