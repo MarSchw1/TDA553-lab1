ddc3cf06e8dd19a7b770bbf12c7f2e7003391405
hannablo2 <hannablo@chalmers.se>
2025-01-23 09:52:17 +0100

    movable och checks

--- car.java
+++ car.java
@@ -1,12 +1,25 @@
 import java.awt.*;
 
-public abstract class car {
+public abstract class car implements movable{
     //Bör alla vara privata för att inte kunna ändras direkt av användaren. De är specifika för bilarna.
     private int nrDoors;
     protected double enginePower;
     private double currentSpeed;
     private Color color;
     private String modelName;
+    protected int direction = 0;
+
+    public void move(){
+
+    }
+
+    public void turnLeft(){
+
+    }
+
+    public void turnRight(){
+
+    }
 
     public car(int nrDoors, double enginePower, Color color, String modelName){
         this.nrDoors = nrDoors;
@@ -27,6 +40,7 @@ public abstract class car {
 
     // protected gömmer funktionerna från utsidan men gör de tillgängliga för subklasser.
     // abstract betyder att subklasser som ex volvo240 can använda denna funktion men att de definierar den unik för varje "fordon".
+    // LÄGG TILL check currentspeed inom [0, enginepower]
     protected abstract double speedFactor();
     protected void incrementSpeed(double amount){
         currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
@@ -36,10 +50,16 @@ public abstract class car {
     }
 
     public void gas(double amount) {
+        if (amount < 0 || amount > 1) {
+            throw new IllegalArgumentException("Not accepted amount for gas, insert a value between 0 and 1");
+        }
         incrementSpeed(amount);
     }
 
     public void brake(double amount) {
+        if (amount < 0 || amount > 1) {
+            throw new IllegalArgumentException("Not accepted amount break, insert a value between 0 and 1");
+        }
         decrementSpeed(amount);
     }
 }
