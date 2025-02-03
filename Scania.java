54141f8964ee9a6491343b20de637b9ba2cea97c
hannablo2 <hannablo@chalmers.se>
2025-02-03 12:18:26 +0100

    workshop

--- Scania.java
+++ Scania.java
@@ -2,18 +2,16 @@ import java.awt.*;
 
 public class Scania extends Truck{
     private double angle;
-    private boolean flakNere;
 
     public Scania () {
         super(2, 100, Color.white, "Scania");
-        flakNere = true;
     }
 
-    private void RaisePlatform(double amount){
+    public void RaisePlatform(double amount){
         angle = Math.min(angle += amount, 70);
     }
 
-    private void LowerPlatform(double amount){
+    public void LowerPlatform(double amount){
         angle = Math.max(angle -= amount, 0);
     }
 
---
+++ Workshop.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Workshop<T extends Car>{
+    private ArrayList<Car> cars;
+    private int capacity;
+
+
+
+
+}
