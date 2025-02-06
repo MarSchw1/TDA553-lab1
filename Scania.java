8a6fd97fbf0c5b89201f8ce66834d230bff9be97
marcelsc <marcelsc@student.chalmers.se>
2025-02-06 10:46:40 +0100

    Påbörjat CarTransport

--- Scania.java
+++ Scania.java
@@ -1,20 +1,32 @@
 import java.awt.*;
 
-public class Scania extends Truck{
+public class Scania extends Car implements HasPlatform{
     private double angle;
 
     public Scania () {
         super(2, 100, Color.white, "Scania");
     }
 
-    public void RaisePlatform(double amount){
+    // Standard raisePlatform för att höja till max.
+    public void raisePlatform() {
+        angle = 70;
+    }
+
+    // Overloada raisPlatform för att kunna höja ett visst antal grader.
+    public void raisePlatform(double amount){
         angle = Math.min(angle += amount, 70);
     }
 
-    public void LowerPlatform(double amount){
+    public void lowerPlatform() {
+        angle = 0;
+    }
+
+    // standard lower platform
+    public void lowerPlatform(double amount){
         angle = Math.max(angle -= amount, 0);
     }
 
+    // overloadad lowerPlatform för att sänka platformen ett viss antal grader
     public double getPlatformAngle(){return angle;}
 
     @Override
